c9669fb147eef8d29b1b11461c22858ec0c38190
ani-sak <animeshsakorikar@gmail.com>
2024-12-16 17:37:30 -0800

    refactor(ringbuffer): cleanup logic

--- src/log.cc
+++ src/log.cc
@@ -31,20 +31,23 @@ Logger::~Logger() {
 
 void Logger::store_logs() {
     while (!stop_log_thread) {
-        RingBuffer<std::string>::Result res = buffer->pop();
-        // Perform formatting before storing
+        // RingBuffer<std::string>::Result res = buffer->try_pop();
+        // // Perform formatting before storing
+        //
+        // if (!res.err()) {
+        //     std::cout << res.data() << '\n';
+        // }
+        //
+        // std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
-        if (!res.err()) {
-            std::cout << res.data() << '\n';
-        }
-
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::string res = buffer->pop();
+        std::cout << res << '\n';
     }
 }
 
 void Logger::log(LogLevel loglevel, const std::string &logmsg) {
     // Prepend time
-    buffer->push(logmsg);
+    buffer->try_push(logmsg);
 }
 
 } // namespace Logging
--- src/ringbuffer.hpp
+++ src/ringbuffer.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <atomic>
+#include <condition_variable>
 #include <cstddef>
 #include <mutex>
 
@@ -14,9 +15,6 @@ public:
     RingBuffer &operator=(RingBuffer &&) = delete;
     RingBuffer &operator=(const RingBuffer &) = delete;
     ~RingBuffer();
-
-    enum class BufferUtilStatus { Empty, Partial, Full };
-    BufferUtilStatus get_buffer_fill_status();
 
     class Result {
     public:
@@ -35,76 +33,92 @@ public:
         bool valid;
     };
 
-    bool push(const T& data); // Fails if queue full
-    // void push_blocking (const T& data); // Blocks if queue full
-    Result pop();
-    // void pop_blocking (const T& data); // Blocks if queue empty
+    bool try_push(const T& data);
+    void push(const T& data);
+    Result try_pop();
+    T pop();
 
 private:
     std::atomic<size_t> head;
     std::atomic<size_t> tail;
+    std::atomic<size_t> num_entries;
 
     const std::size_t buffer_size;
     T *buf;
 
     std::mutex push_mtx;
     std::mutex pop_mtx;
+
+    std::condition_variable cv_buf_not_full;
+    std::condition_variable cv_buf_not_empty;
 };
 
 //===----------------------------------------------------------------------===//
 
 template <typename T>
 RingBuffer<T>::RingBuffer(size_t buffer_size)
-    : head(0), tail(0), buffer_size(buffer_size + 1), buf(nullptr), push_mtx(),
-      pop_mtx() {
-    buf = new T[this->buffer_size];
+    : head(0), tail(0), num_entries(0), buffer_size(buffer_size),
+      buf(nullptr), push_mtx(), pop_mtx() {
+    buf = new T[buffer_size];
 }
 
 template <typename T> RingBuffer<T>::~RingBuffer() { delete[] buf; }
 
-template <typename T>
-typename RingBuffer<T>::BufferUtilStatus RingBuffer<T>::get_buffer_fill_status() {
-    if (head == tail) {
-        return BufferUtilStatus::Empty;
-    }
+template <typename T> bool RingBuffer<T>::try_push(const T& data) {
+    std::lock_guard<std::mutex> lock(push_mtx);
 
-    if ((tail + 1) % buffer_size == head) {
-        return BufferUtilStatus::Full;
-    }
-
-    return BufferUtilStatus::Partial;
-}
-
-template <typename T> bool RingBuffer<T>::push(const T& data) {
-    switch (get_buffer_fill_status()) {
-    case BufferUtilStatus::Empty:
-    case BufferUtilStatus::Partial: {
-        std::lock_guard<std::mutex> lock(push_mtx);
-        buf[tail] = data;
-        tail = (tail + 1) % buffer_size;
-        return true;
-    }
-    case BufferUtilStatus::Full: {
+    if (num_entries >= buffer_size) {
         return false;
     }
-    default:
-        return false;
-    }
+
+    buf[tail] = data;
+    tail = (tail + 1) % buffer_size;
+    num_entries++;
+
+    cv_buf_not_empty.notify_one();
+    return true;
 }
 
-template <typename T> typename RingBuffer<T>::Result RingBuffer<T>::pop() {
-    switch (get_buffer_fill_status()) {
-    case BufferUtilStatus::Empty: {
+template <typename T> void RingBuffer<T>::push(const T& data) {
+    std::unique_lock<std::mutex> lock(push_mtx);
+
+    if (num_entries >= buffer_size) {
+        cv_buf_not_full.wait(lock,
+                             [this] { return num_entries < buffer_size; });
+    }
+
+    buf[tail] = data;
+    tail = (tail + 1) % buffer_size;
+    num_entries++;
+
+    cv_buf_not_empty.notify_one();
+    return;
+}
+
+template <typename T> typename RingBuffer<T>::Result RingBuffer<T>::try_pop() {
+    std::lock_guard<std::mutex> lock(pop_mtx);
+
+    if (num_entries <= 0) {
         return {T{}, false};
     }
-    case BufferUtilStatus::Partial:
-    case BufferUtilStatus::Full: {
-        std::lock_guard<std::mutex> lock(pop_mtx);
-        std::size_t old_head = head;
-        head = (head + 1) % buffer_size;
-        return {buf[old_head], true};
+
+    std::size_t old_head = head;
+    head = (head + 1) % buffer_size;
+    num_entries--;
+    cv_buf_not_full.notify_one();
+    return {buf[old_head], true};
+}
+
+template <typename T> T RingBuffer<T>::pop() {
+    std::unique_lock<std::mutex> lock(pop_mtx);
+
+    if (num_entries == 0) {
+        cv_buf_not_empty.wait(lock, [this] { return num_entries > 0; });
     }
-    default:
-        return {T{}, false};
-    }
+
+    std::size_t old_head = head;
+    head = (head + 1) % buffer_size;
+    num_entries--;
+    cv_buf_not_full.notify_one();
+    return buf[old_head];
 }
