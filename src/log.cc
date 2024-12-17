deb49f6d6cad2ef782b07219edeec25b04745db1
ani-sak <animeshsakorikar@gmail.com>
2024-12-16 16:05:34 -0800

    feat: logger class

---
+++ src/log.cc
@@ -0,0 +1,50 @@
+#include "log.hpp"
+#include "ringbuffer.hpp"
+
+#include <chrono>
+#include <cstddef>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <thread>
+
+namespace Logging {
+
+constexpr std::size_t default_queue_size = 250;
+
+Logger::Logger()
+    : buffer(std::make_unique<RingBuffer<std::string>>(default_queue_size)),
+      stop_log_thread(false), log_thread(&Logger::store_logs, this) {}
+
+Logger::Logger(std::size_t queue_size)
+    : buffer(std::make_unique<RingBuffer<std::string>>(queue_size)),
+      stop_log_thread(false), log_thread(&Logger::store_logs, this) {}
+
+Logger::~Logger() {
+    // Flush buffer
+
+    stop_log_thread = true;
+    if (log_thread.joinable()) {
+        log_thread.join();
+    }
+}
+
+void Logger::store_logs() {
+    while (!stop_log_thread) {
+        RingBuffer<std::string>::Result res = buffer->pop();
+        // Perform formatting before storing
+
+        if (!res.err()) {
+            std::cout << res.data() << '\n';
+        }
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+}
+
+void Logger::log(LogLevel loglevel, const std::string &logmsg) {
+    // Prepend time
+    buffer->push(logmsg);
+}
+
+} // namespace Logging
---
+++ src/log.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+//===----------------------------------------------------------------------===//
+//
+// Non singleton async logger
+// - Allow user to decide no of instances
+// - Permits easier testing
+//===----------------------------------------------------------------------===//
+
+#include <cstddef>
+#include <memory>
+#include <string>
+#include <thread>
+
+#include "ringbuffer_fwd.hpp"
+
+namespace Logging {
+
+class Logger {
+public:
+    Logger(std::size_t queue_size);
+    Logger();
+    Logger(Logger &&) = delete;
+    Logger(const Logger &) = delete;
+    Logger &operator=(Logger &&) = delete;
+    Logger &operator=(const Logger &) = delete;
+    ~Logger();
+
+    enum class LogLevel { Debug, Warn, Error };
+    void log(LogLevel loglevel, const std::string &logmsg);
+
+private:
+    std::unique_ptr<RingBuffer<std::string>> buffer;
+    bool stop_log_thread;
+    void store_logs();
+    std::thread log_thread;
+};
+
+} // namespace Logging
