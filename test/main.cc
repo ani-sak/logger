653533f26e98dffa9cb8a48b206f7c3697193082
ani-sak <animeshsakorikar@gmail.com>
2025-02-13 16:54:29 -0800

    feat(ringbuffer): support rvalue types using universal refs and perfect-forwarding

--- test/main.cc
+++ test/main.cc
@@ -68,6 +68,7 @@ void test_file_logger() {
 auto main(int argc, char* argv[]) -> int {
     if (argc != 2) {
         fmt::print("Incorrent arguments provided.\n");
+        return 1;
     }
 
     if (std::string(argv[1]) == "c") {
