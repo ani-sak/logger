f02b7e18408c8c4e9ed915487b4611ec1dc879d2
ani-sak <animeshsakorikar@gmail.com>
2025-04-11 08:56:23 -0700

    Fix data races in console/file logger factory functions (#11)

--- test/main.cc
+++ test/main.cc
@@ -1,17 +1,21 @@
 #include "async_logger/logger.hpp"
 #include "fmt/base.h"
-#include "fmt/format.h"
 #include "fmt/chrono.h"
+#include "fmt/format.h"
 
+#include <array>
 #include <chrono>
 #include <cstddef>
+#include <iterator>
 #include <memory>
 #include <string>
 #include <thread>
 
-void logger_test_helper(const std::shared_ptr<Logger::Logger>& logger, std::size_t log_rounds,
-                 std::size_t logs_per_round,
-                 Logger::LogLevel log_level = Logger::LogLevel::Warn) {
+namespace Test {
+
+void logger_test_helper(const std::shared_ptr<Logger::Logger>& logger,
+                        std::size_t log_rounds, std::size_t logs_per_round,
+                        Logger::LogLevel log_level = Logger::LogLevel::Warn) {
     const auto start_time = std::chrono::high_resolution_clock::now();
 
     for (std::size_t rnd = 0; rnd < log_rounds; rnd++) {
@@ -28,56 +32,62 @@ void logger_test_helper(const std::shared_ptr<Logger::Logger>& logger, std::size
                (end_time - start_time));
 }
 
-void test_console_logger() {
-    constexpr std::size_t buffer_size = 6;
-    constexpr std::size_t logs_per_round = 10;
+namespace {
+constexpr std::size_t buffer_size = 5;
+constexpr std::size_t logs_per_round = 30;
+constexpr std::size_t log_rounds = 3;
+} // namespace
 
-    auto cout_1 =
-        Logger::ConsoleLogger(buffer_size, Logger::LogStrategy::Blocking);
-    auto cout_2 = Logger::ConsoleLogger(buffer_size);
-
-    std::thread console_log_thread_1(&logger_test_helper, cout_1, 3,
-                                     logs_per_round, Logger::LogLevel::Error);
-    std::thread console_log_thread_2(&logger_test_helper, cout_2, 3,
-                                     logs_per_round, Logger::LogLevel::Warn);
-
-    console_log_thread_1.join();
-    console_log_thread_2.join();
+void test_console_logger(Logger::LogStrategy ls, Logger::LogLevel ll) {
+    auto cout_1 = Logger::ConsoleLogger(buffer_size, ls);
+    logger_test_helper(cout_1, log_rounds, logs_per_round, ll);
 }
 
-void test_file_logger() {
-    constexpr std::size_t buffer_size = 6;
-    constexpr std::size_t logs_per_round = 50;
-
-    auto filelog_1 = Logger::FileLogger("testlog.log", buffer_size);
-    auto filelog_2 = Logger::FileLogger("testlog.log", buffer_size);
-    auto filelog_3 = Logger::FileLogger("testlog.log", buffer_size);
-
-    std::thread file_log_thread_1(&logger_test_helper, filelog_1, 3,
-                                  logs_per_round, Logger::LogLevel::Error);
-    std::thread file_log_thread_2(&logger_test_helper, filelog_2, 3,
-                                  logs_per_round, Logger::LogLevel::Warn);
-    std::thread file_log_thread_3(&logger_test_helper, filelog_3, 3,
-                                  logs_per_round, Logger::LogLevel::Debug);
-
-    file_log_thread_1.join();
-    file_log_thread_2.join();
-    file_log_thread_3.join();
+void test_file_logger(Logger::LogStrategy ls, Logger::LogLevel ll) {
+    auto filelog_1 = Logger::FileLogger("testlog.log", buffer_size, ls);
+    logger_test_helper(filelog_1, log_rounds, logs_per_round, ll);
 }
 
-auto main(int argc, char* argv[]) -> int {
-    if (argc != 2) {
-        fmt::print("Incorrent arguments provided.\n");
-        return 1;
+} // namespace Test
+
+auto get_level(std::size_t i) -> Logger::LogLevel {
+    switch (i % 3) {
+    case 0:
+        return Logger::LogLevel::Debug;
+    case 1:
+        return Logger::LogLevel::Warn;
+    case 2:
+        return Logger::LogLevel::Error;
+    default:
+        Logger::LogLevel::Error;
+    }
+}
+
+auto main(int  /*argc*/, char*  /*argv*/[]) -> int {
+    constexpr int num_threads_test_console_logger = 3;
+    constexpr int num_threads_test_file_logger = 3;
+
+    std::array<std::thread, num_threads_test_console_logger>
+        console_logger_threads;
+    std::array<std::thread, num_threads_test_file_logger> file_logger_threads;
+
+    constexpr Logger::LogStrategy test_strat = Logger::LogStrategy::Blocking;
+
+    for (std::size_t i = 0; i < num_threads_test_console_logger; ++i) {
+        console_logger_threads[i] =
+            std::thread{&Test::test_console_logger, test_strat, get_level(i)};
     }
 
-    if (std::string(argv[1]) == "c") {
-        test_console_logger();
+    for (std::size_t i = 0; i < num_threads_test_file_logger; ++i) {
+        file_logger_threads[i] =
+            std::thread{&Test::test_file_logger, test_strat, get_level(i)};
     }
 
-    else if (std::string(argv[1]) == "f") {
-        test_file_logger();
+    for (std::size_t i = 0; i < num_threads_test_console_logger; ++i) {
+        console_logger_threads[i].join();
     }
 
-    return 0;
+    for (std::size_t i = 0; i < num_threads_test_file_logger; ++i) {
+        file_logger_threads[i].join();
+    }
 }
