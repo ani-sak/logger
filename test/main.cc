e626b5b74b060022a3b33fb75a70585dc6800e38
ani-sak <animeshsakorikar@gmail.com>
2025-07-17 14:21:02 -0700

    test: test correct ANSI codes are output to terminal

--- test/main.cc
+++ test/main.cc
@@ -5,6 +5,7 @@
 
 #include <fstream>
 #include <string>
+#include <string_view>
 
 namespace {
 template <typename F>
@@ -68,6 +69,13 @@ auto main(int /*argc*/, char* /*argv*/[]) -> int {
 
         AsyncLogger::flush(buf_term);
 
+        // Verify correct ANSI codes to color error/warn logs
+        std::string debug_yellow_msg = "Warn Yellow";
+        const char * error_red_msg = "Error Red";
+        AsyncLogger::log(buf_term, AsyncLogger::LogLevel::Warn, debug_yellow_msg);
+        AsyncLogger::log(buf_term, AsyncLogger::LogLevel::Error, error_red_msg);
+        AsyncLogger::flush(buf_term);
+
         // Verify setting log level is adhered to
         {
             AsyncLogger::set_log_level(AsyncLogger::LogLevel::Warn);
@@ -89,6 +97,8 @@ auto main(int /*argc*/, char* /*argv*/[]) -> int {
             AsyncLogger::log(buf_term, AsyncLogger::LogLevel::Warn, "NO SHOW");
             AsyncLogger::log(buf_term, AsyncLogger::LogLevel::Error, "SHOW");
         }
+
+        AsyncLogger::flush(buf_term);
 
         // TODO
         // Verify log entry longer than buffer entry size clipped
@@ -199,14 +209,53 @@ auto main(int /*argc*/, char* /*argv*/[]) -> int {
     fmt::print(
         fmt::format(success_style, "Buffer does not overwrite test passed \n"));
 
+    std::string ansi_reset = "\033[0m";
+    std::string ansi_rgb_prefix = "\033[38;2;";
+    std::string ansi_rgb_red = "255;000;000";
+    std::string ansi_rgb_yellow = "255;255;000";
+    std::string ansi_cmd_posfix = "m";
+
+    std::string debug_yellow_msg = "Warn Yellow";
+    std::string error_red_msg = "Error Red";
+
+    if (std::getline(ifs, line)) {
+        std::string ver = ansi_rgb_prefix + ansi_rgb_yellow + ansi_cmd_posfix +
+                          debug_yellow_msg;
+        if (line != ver) {
+            fmt::print(error_style, "Display WARN in terminal test failed \n");
+            return 1;
+        }
+        fmt::print(fmt::format(success_style,
+                               "Display WARN in terminal test passed \n"));
+    }
+    if (std::getline(ifs, line)) {
+        std::string ver = ansi_reset + ansi_rgb_prefix + ansi_rgb_red +
+                          ansi_cmd_posfix + error_red_msg;
+        if (line != ver) {
+            fmt::print(error_style, "Display ERROR in terminal test failed \n");
+            return 1;
+        }
+        fmt::print(fmt::format(success_style,
+                               "Display ERROR in terminal test passed \n"));
+    }
+
     constexpr std::size_t set_log_level_warn_test_entries = 2;
     constexpr std::size_t set_log_level_error_test_entries = 1;
 
+    std::string show_msg = "SHOW";
+
     count = 0;
     while (count < set_log_level_warn_test_entries && std::getline(ifs, line)) {
-        if (line != "SHOW") {
-            fmt::print(error_style, "Set log level adhered test failed \n",
-                       count);
+        std::string ver = ansi_reset + ansi_rgb_prefix + ansi_rgb_yellow +
+                          ansi_cmd_posfix + show_msg;
+        if (count >= 1) {
+            ver = ansi_reset + ansi_rgb_prefix + ansi_rgb_red +
+                  ansi_cmd_posfix + show_msg;
+        }
+        if (line != ver) {
+            fmt::print(error_style,
+                       "Set log level WARN adhered test [{}/{}] failed \n",
+                       (count+1), set_log_level_warn_test_entries);
             return 1;
         }
         ++count;
@@ -217,9 +266,11 @@ auto main(int /*argc*/, char* /*argv*/[]) -> int {
     count = 0;
     while (count < set_log_level_error_test_entries &&
            std::getline(ifs, line)) {
-        if (line != "SHOW") {
-            fmt::print(error_style, "Set log level adhered test failed \n",
-                       count);
+        std::string ver = ansi_reset + ansi_rgb_prefix + ansi_rgb_red +
+                          ansi_cmd_posfix + show_msg;
+        if (line != ver) {
+            fmt::print(error_style,
+                       "Set log level ERROR adhered test failed \n", count);
             return 1;
         }
         ++count;
@@ -227,8 +278,6 @@ auto main(int /*argc*/, char* /*argv*/[]) -> int {
     fmt::print(fmt::format(success_style,
                            "Set log level ERROR adhered test passed \n"));
 
-
-    fmt::print(fmt::format(success_style,
-                           "\nAll tests passed!\n"));
+    fmt::print(fmt::format(success_style, "\nAll tests passed!\n"));
     return 0;
 }
